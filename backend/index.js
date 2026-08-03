9a0af9559e3ce1b420b052b562e71f195a8b4c42
Ayush <ayush06012006@gmail.com>
2026-08-03 11:54:46 +0530

    Fetching holdings and positions data from DB

--- backend/index.js
+++ backend/index.js
@@ -9,49 +9,61 @@ const uri = process.env.MONGO_URL;
 
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser=require('body-parser');
+const cors=require('cors');
 const {PositionsModel}=require('./model/PositionsModel');
+const { HoldingsModel } = require('./model/HoldingsModel');
 const app=express();
 
-app.get('/addPositions',(req,res)=>{
-  let tempPositions=[
-  {
-    product: "CNC",
-    name: "EVEREADY",
-    qty: 2,
-    avg: 316.27,
-    price: 312.35,
-    net: "+0.58%",
-    day: "-1.24%",
-    isLoss: true,
-  },
-  {
-    product: "CNC",
-    name: "JUBLFOOD",
-    qty: 1,
-    avg: 3124.75,
-    price: 3082.65,
-    net: "+10.04%",
-    day: "-1.35%",
-    isLoss: true,
-  },
-];
+app.use(cors());
+app.use(bodyParser.json());
 
-  tempPositions.forEach((item)=>{
+//  app.get('/addPositions',(req,res)=>{
+//    let tempPositions=[
+//    {
+//      product: "CNC",
+//      name: "EVEREADY",
+//      qty: 2,
+//      avg: 316.27,
+//      price: 312.35,
+//      net: "+0.58%",
+//      day: "-1.24%",
+//      isLoss: true,
+//    },
+//    {
+//      product: "CNC",
+//      name: "JUBLFOOD",
+//      qty: 1,
+//      avg: 3124.75,
+//      price: 3082.65,
+//      net: "+10.04%",
+//      day: "-1.35%",
+//      isLoss: true,
+//    },
+//  ]
+//    tempPositions.forEach((item)=>
+//      let newPosition=new PositionsModel({
+//        product: item.product,
+//        name: item.name,
+//        qty: item.qty,
+//        avg: item.avg,
+//        price: item.price,
+//        net: item.net,
+//        day: item.day,
+//        isLoss: item.isLoss,
+//      }
+//      newPosition.save();
+//    });
+//   res.send("Done");
+// })
 
-    let newPosition=new PositionsModel({
-      product: item.product,
-      name: item.name,
-      qty: item.qty,
-      avg: item.avg,
-      price: item.price,
-      net: item.net,
-      day: item.day,
-      isLoss: item.isLoss,
-    })
-
-    newPosition.save();
-  });
-  res.send("Done");
+app.get('/allHoldings', async (req,res)=>{
+  let allHoldings=await HoldingsModel.find({});
+  res.json(allHoldings);
+})
+app.get('/allPositions', async (req,res)=>{
+  let allPositions=await PositionsModel.find({});
+  res.json(allPositions);
 })
 
 
